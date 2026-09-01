e40b5b4b8e651588e05446f637f2bbef65df0571
Didier Daloze <dalozedidier@gmail.com>
2026-09-01 04:56:57 +0200

    Bump package version to 0.8.6

--- src/mcs/__init__.py
+++ src/mcs/__init__.py
@@ -79,7 +79,7 @@ from .robustness import (
 )
 from .simulator import SimConfig, SimResult, simulate
 
-__version__ = "0.8.5"
+__version__ = "0.8.6"
 __all__ = [
     "DEFAULT_THRESHOLDS",
     "HysteresisClassifier",
